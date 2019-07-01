0e72bde8132823764e41983f70da4f2d562d64bf
Juliet-Selebalo <31500113+Juliet-Selebalo@users.noreply.github.com>
2019-07-01 21:04:52 +0400

    Final files upload

---
+++ Default.aspx
@@ -0,0 +1,37 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="MP3Thumbnails._Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>MP3 Stream</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <asp:ScriptManager ID="sm1" runat="server" />
+    <div>
+        Upload mp3:
+            <asp:FileUpload ID="upload" runat="server" />
+            <asp:Button ID="submitButton" runat="server" Text="Submit" OnClick="submitButton_Click" />
+    </div>
+    <div>
+            <asp:UpdatePanel ID="up1" runat="server">
+                <ContentTemplate>
+                    <asp:ListView ID="MP3SamplesDisplayControl" runat="server">
+                        <LayoutTemplate>
+                            <div id="itemPlaceholder" runat="server" />
+                        </LayoutTemplate>
+                        <ItemTemplate>
+                            <div>
+                                <asp:Literal ID="label" Text='<%# Eval("Title") %>' runat="server"/>
+                                <audio src='<%# Eval("Url") %>' controls="" preload="none"></audio>
+                            </div>
+                        </ItemTemplate>
+                    </asp:ListView>
+                    <asp:Button ID="refreshButton" runat="server" Text="Refresh" OnClick="Page_PreRender"/>
+                </ContentTemplate>
+            </asp:UpdatePanel>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Functions.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+using Microsoft.Azure.WebJobs;
+using Microsoft.WindowsAzure.Storage.Blob;
+using NAudio.Wave;
+
+namespace MP3Thumbnails_WebJob
+{
+    public class Functions
+    {
+
+      public static void GenerateThumbnail(
+        [QueueTrigger("mp3SampleMaker")] String blobInfo,
+        [Blob("audio/mp3Files/{queueTrigger}")] CloudBlockBlob inputBlob,
+        [Blob("audio/samples/{queueTrigger}")] CloudBlockBlob outputBlob, TextWriter logger)
+        {
+
+            //set duration of sample to 20 seconds
+            int duration = 20;
+
+            //use log.WriteLine() rather than Console.WriteLine() for trace output
+            logger.WriteLine("CreateMP3Sample() started...");
+            logger.WriteLine("Input blob is: " + blobInfo);
+
+            // Open streams to blobs for reading and writing as appropriate.
+            // Pass references to application specific methods
+            using (Stream input = inputBlob.OpenRead())
+            using (Stream output = outputBlob.OpenWrite())
+            {
+                CreateMP3Sample(input, output, duration);
+                outputBlob.Properties.ContentType = "audio/mpeg3.";
+                outputBlob.Metadata["Title"] = inputBlob.Metadata["Title"];
+            }
+            logger.WriteLine("CreateMP3Sample() completed...");
+        }
+
+        // Create thumbnail - the detail is unimportant but notice formal parameter types.
+
+        private static void CreateMP3Sample(Stream input, Stream output, int duration)
+          {
+            using (var reader = new Mp3FileReader(input, wave => new NLayer.NAudioSupport.Mp3FrameDecompressor(wave)))
+            {
+                Mp3Frame frame;
+                frame = reader.ReadNextFrame();
+                int frameTimeLength = (int)(frame.SampleCount / (double)frame.SampleRate * 1000.0);
+                int framesRequired = (int)(duration / (double)frameTimeLength * 1000.0);
+
+                int frameNumber = 0;
+                while ((frame = reader.ReadNextFrame()) != null)
+                {
+                    frameNumber++;
+
+                    if (frameNumber <= framesRequired)
+                    {
+                        output.Write(frame.RawData, 0, frame.RawData.Length);
+                    }
+                    else break;
+                }
+            }
+        }
+    }
+}
